816be8e3033f4846a39d94c9adc88bf0a2345911
GellyBird <147560865+GellyBird@users.noreply.github.com>
2025-03-11 16:29:10 +0000

    final push i think woohooo

--- src/main/java/com/example/project/test.java
+++ src/main/java/com/example/project/test.java
@@ -1,8 +1,8 @@
 package com.example.project;
 public class test {
     public static void main(String[] args) {
-        String message = "ur=oe=yv!  eesdriia s ttwiuo oh?  yeoacldilonet H y";
-        int rows = 7;
+        String message = "IGY  MZNN=H USIA EA=";
+        int rows = 10;
         System.out.println(Encryptor.decryptMessage(message, rows));
 
     }
